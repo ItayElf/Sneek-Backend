3ab79a5a8dd94f6719d6f4f66866dcdb2ba1ccc7
ItayElf <thh123321@gmail.com>
2023-12-28 15:39:18 +0200

    Feat: added channel join

--- web/api/channels.py
+++ web/api/channels.py
@@ -3,7 +3,8 @@ A file for sending messages in channels
 """
 from typing import Any, Dict, List
 
-from flask import jsonify
+from flask import jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from web import database, User, app
 
@@ -48,3 +49,19 @@ def create_channels(channels: List[Dict[str, Any]]):
 def get_channels():
     channels = Channel.query.all()
     return jsonify([channel.serialize() for channel in channels])
+
+
+@app.route("/api/channels/join", methods=["POST"])
+@jwt_required()
+def join_channel():
+    username = get_jwt_identity()
+    user = User.query.filter_by(name=username).first()
+    if not user:
+        return f"User {username} was not found", 404
+    channel_name = request.json.get("channel", "")
+    channel = Channel.query.filter_by(name=channel_name).first()
+    if not channel:
+        return f"Channel {channel_name} does not exists", 404
+    user.connected_to = channel_name
+    database.session.commit()
+    return "", 200
