5c0682ed07b90296f158e1da21135160f0f35f89
ItayElf <thh123321@gmail.com>
2023-12-28 11:49:23 +0200

    Feat: added db

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 """
 The main file
 """
+import pathlib
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+home_directory = pathlib.Path(__file__).parent.absolute()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{home_directory / 'sneek.db'}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route("/")
@@ -12,5 +21,12 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-if __name__ == '__main__':
+def main():
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
     app.run()
+
+
+if __name__ == '__main__':
+    main()
