12dc13f744330a0522245d53096c400408fb4935
ItayElf <thh123321@gmail.com>
2023-12-28 12:36:00 +0200

    Feat: added configuration

--- web/base.py
+++ web/base.py
@@ -5,16 +5,22 @@ This file is needed so that the app and the database could be accessible in othe
 """
 import pathlib
 import secrets
+from datetime import timedelta
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-DATABASE_LOCATION = pathlib.Path(__file__).parent.parent / "sneek.db"
+from web.configuration import Configuration
+
+_DATABASE_LOCATION = pathlib.Path(__file__).parent.parent / "sneek.db"
+CONFIGURATION = Configuration.load()
 
 app = Flask(__name__)
 
 app.config["JWT_SECRET_KEY"] = secrets.token_hex()
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DATABASE_LOCATION}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=CONFIGURATION.session_duration_in_hours)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{_DATABASE_LOCATION}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 database = SQLAlchemy(app)
---
+++ web/configuration.py
@@ -0,0 +1,27 @@
+"""
+A file for the configuration of the Sneek backend
+"""
+import json
+import pathlib
+from dataclasses import dataclass
+
+from typing_extensions import List
+
+CONFIGURATION_FILENAME = pathlib.Path(__file__).parent.parent / "config.json"
+
+
+@dataclass(frozen=True)
+class Configuration:
+    """
+    A class that holds the data for the Sneek configuration
+    """
+    session_duration_in_hours: int
+    name_adjectives: List[str]
+    name_animals: List[str]
+
+    @classmethod
+    def load(cls) -> "Configuration":
+        """
+        Loads the configuration from the configuration file
+        """
+        return cls(**json.loads(CONFIGURATION_FILENAME.read_text("utf-8")))
