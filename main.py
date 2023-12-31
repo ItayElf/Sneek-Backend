cbe652858520d9c654c46c3fc1aa6b3966d8eaac
ItayElf <thh123321@gmail.com>
2023-12-31 18:35:34 +0200

    Feat: added route for frontend

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 """
 The main file
 """
+
 from web import create_channels
 from web.base import app, database, CONFIGURATION
-
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
 
 def main():
--- web/base.py
+++ web/base.py
@@ -3,20 +3,22 @@ A file for the configuration of the flask app
 
 This file is needed so that the app and the database could be accessible in other files
 """
+import os
 import pathlib
 import secrets
 from datetime import timedelta
 
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 
 from web.configuration import Configuration
 
 _DATABASE_LOCATION = pathlib.Path(__file__).parent.parent / "sneek.db"
+_STATIC_FOLDER = pathlib.Path(__file__).parent.parent / "static"
 CONFIGURATION = Configuration.load()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=str(_STATIC_FOLDER), static_url_path="/")
 
 app.config["JWT_SECRET_KEY"] = secrets.token_hex()
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=CONFIGURATION.session_duration_in_hours)
@@ -26,6 +28,16 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 database = SQLAlchemy(app)
 jwt = JWTManager(app)
+
+
+@app.route("/", defaults={'path': ''})
+@app.route("/<path:path>")
+def serve(path):
+    print(repr(path), app.static_url_path, app.static_folder)
+    if path and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, "index.html")
 
 
 @jwt.expired_token_loader
